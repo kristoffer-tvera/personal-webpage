8f46cc0c3ff114e2c691aada7b2a5f20383b841b
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-03-10 16:22:08 +0100

    Updated browser compatability css, and service worker registration

--- dist/sw.js
+++ dist/sw.js
@@ -1,107 +1,42 @@
 // Set a name for the current cache
-var cacheName = 'v1'; 
+var cacheName = 'v1';
 
 // Default files to always cache
 var cacheFiles = [
 	'./',
-	'./index.html',
-	'./script/site.min.js',
-	'./styling/site.min.css',
-	'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
-	'https://fonts.googleapis.com/css?family=Playfair+Display'
+	'./about',
+	'./experience',
+	'./projects',
+	'./contact',
+	'./assets/site.min.css',
+	'./assets/site.min.js',
+	'./images',
+	'https://fonts.googleapis.com/css?family=Roboto'
 ]
 
+self.addEventListener('install', function (evt) {
+	evt.waitUntil(precache());
+});
 
-self.addEventListener('install', function(e) {
-    console.log('[ServiceWorker] Installed');
-
-    // e.waitUntil Delays the event until the Promise is resolved
-    e.waitUntil(
-
-    	// Open the cache
-	    caches.open(cacheName).then(function(cache) {
-
-	    	// Add all the default files to the cache
-			console.log('[ServiceWorker] Caching cacheFiles');
-			return cache.addAll(cacheFiles);
-	    })
-	); // end e.waitUntil
+self.addEventListener('fetch', function (evt) {
+	evt.respondWith(fromCache(evt.request));
+	evt.waitUntil(update(evt.request));
 });
 
 
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activated');
+async function precache() {
+	const cache = await caches.open(cacheName);
+	return cache.addAll(cacheFiles);
+}
 
-    e.waitUntil(
+async function fromCache(request) {
+	const cache = await caches.open(cacheName);
+	const matching = await cache.match(request);
+	return matching || Promise.reject('no-match');
+}
 
-    	// Get all the cache keys (cacheName)
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(cacheNames.map(function(thisCacheName) {
-
-				// If a cached item is saved under a previous cacheName
-				if (thisCacheName !== cacheName) {
-
-					// Delete that cached file
-					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-					return caches.delete(thisCacheName);
-				}
-			}));
-		})
-	); // end e.waitUntil
-
-});
-
-
-self.addEventListener('fetch', function(e) {
-	console.log('[ServiceWorker] Fetch', e.request.url);
-
-	// e.respondWidth Responds to the fetch event
-	e.respondWith(
-
-		// Check in cache for the request being made
-		caches.match(e.request)
-
-
-			.then(function(response) {
-
-				// If the request is in the cache
-				if ( response ) {
-					console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-					// Return the cached version
-					return response;
-				}
-
-				// If the request is NOT in the cache, fetch and cache
-
-				var requestClone = e.request.clone();
-				return fetch(requestClone)
-					.then(function(response) {
-
-						if ( !response ) {
-							console.log("[ServiceWorker] No response from fetch ")
-							return response;
-						}
-
-						var responseClone = response.clone();
-
-						//  Open the cache
-						caches.open(cacheName).then(function(cache) {
-
-							// Put the fetched response in the cache
-							cache.put(e.request, responseClone);
-							console.log('[ServiceWorker] New Data Cached', e.request.url);
-
-							// Return the response
-							return response;
-			
-				        }); // end caches.open
-
-					})
-					.catch(function(err) {
-						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-					});
-
-
-			}) // end caches.match(e.request)
-	); // end e.respondWith
-});
+async function update(request) {
+	const cache = await caches.open(CACHE);
+	const response = await fetch(request);
+	return cache.put(request, response);
+}
